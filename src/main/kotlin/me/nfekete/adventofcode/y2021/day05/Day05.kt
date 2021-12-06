4231bea3fb17f227c394d96a6da81ad626eebeb0
Nándor Előd Fekete <nandor.fekete@gmail.com>
2021-12-07 00:42:58 +0100

    Day 5 part 2

--- src/main/kotlin/me/nfekete/adventofcode/y2021/day05/Day05.kt
+++ src/main/kotlin/me/nfekete/adventofcode/y2021/day05/Day05.kt
@@ -1,12 +1,17 @@
 package me.nfekete.adventofcode.y2021.day05
 
 import me.nfekete.adventofcode.y2021.common.classpathFile
+import kotlin.math.abs
 
 private data class Point(val x: Int, val y: Int)
 private data class Line(val start: Point, val end: Point) {
     companion object
+
     fun isHorizontal() = start.y == end.y
     fun isVertical() = start.x == end.x
+    fun isDiagonal() = abs(start.x - end.x) == abs(start.y - end.y)
+    val xRange get() = if (start.x <= end.x) start.x..end.x else start.x downTo end.x
+    val yRange get() = if (start.y <= end.y) start.y..end.y else start.y downTo end.y
 }
 
 private val lineRegex = Regex("(\\d+),(\\d+) -> (\\d+),(\\d+)")
@@ -18,36 +23,42 @@ private fun Line.Companion.parse(line: String) =
             Line(Point(x1.toInt(), y1.toInt()), Point(x2.toInt(), y2.toInt()))
         }
 
-private class Raster(private val width: Int, private val height: Int) {
-    private val points = IntArray(width * height)
+private fun List<Line>.pointsOfHorizontalLines() =
+    filter { it.isHorizontal() }
+        .flatMap { horizontalLine ->
+            horizontalLine.xRange.map { Point(it, horizontalLine.start.y) }
+        }
 
-    fun inc(x: Int, y: Int) = points[y * width + x]++
-    fun count(predicate: (Int) -> Boolean) = points.count(predicate)
-}
+private fun List<Line>.pointsOfVerticalLines() =
+    filter { it.isVertical() }
+        .flatMap { verticalLine ->
+            verticalLine.yRange.map { Point(verticalLine.start.x, it) }
+        }
 
-private fun inOrder(a: Int, b: Int) = if (a < b) a to b else b to a
-private fun List<Line>.maxX() = flatMap { listOf(it.start.x, it.end.x) }.maxOrNull()!!
-private fun List<Line>.maxY() = flatMap { listOf(it.start.y, it.end.y) }.maxOrNull()!!
+private fun List<Line>.pointsOfDiagonalLines() =
+    filter { it.isDiagonal() }
+        .flatMap { diagonalLine ->
+            diagonalLine.xRange.zip(diagonalLine.yRange, ::Point)
+        }
+
+private fun List<Point>.countOverlaps() =
+    groupingBy { it }
+        .eachCount()
+        .values.count { it >= 2 }
 
 private fun main() {
     val input = classpathFile("day05/input.txt")
         .readLines()
         .map { Line.parse(it) }
 
-    val raster = Raster(input.maxX() + 1, input.maxY() + 1)
-    input.filter { it.isVertical() }
-        .flatMap { verticalLine ->
-            val (startY, endY) = inOrder(verticalLine.start.y, verticalLine.end.y)
-            (startY..endY).map { verticalLine.start.x to it }
-        }
-        .plus(
-            input.filter { it.isHorizontal() }
-                .flatMap { horizontalLine ->
-                    val (startX, endX) = inOrder(horizontalLine.start.x, horizontalLine.end.x)
-                    (startX..endX).map { it to horizontalLine.start.y }
-                }
-        )
-        .forEach { (x, y) -> raster.inc(x, y) }
+    val part1Lines = input.pointsOfVerticalLines()
+        .plus(input.pointsOfHorizontalLines())
 
-    raster.count { it >= 2 }.let { println("Part1: $it") }
+    part1Lines
+        .countOverlaps()
+        .let { println("Part1: $it") }
+
+    part1Lines.plus(input.pointsOfDiagonalLines())
+        .countOverlaps()
+        .let { println("Part2: $it") }
 }
