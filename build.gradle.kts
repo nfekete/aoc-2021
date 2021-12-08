7f701cb5920458bef019ad6eb93c85dafbafe711
Nándor Előd Fekete <nandor.fekete@gmail.com>
2021-12-08 17:36:08 +0100

    Day 8

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,22 +12,22 @@ repositories {
     mavenCentral()
 }
 
-val kotestVersion = "4.3.2"
+val kotestVersion = "4.6.3"
 
 dependencies {
-    implementation("org.junit.jupiter:junit-jupiter:5.4.2")
-    testImplementation(kotlin("test-junit5"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
-    testImplementation ("io.kotest:kotest-runner-junit5:${kotestVersion}")
-    testImplementation ("io.kotest:kotest-assertions-core:${kotestVersion}")
+    implementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    implementation(kotlin("test-junit5"))
+    implementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    runtimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    implementation ("io.kotest:kotest-runner-junit5:${kotestVersion}")
+    implementation ("io.kotest:kotest-assertions-core:${kotestVersion}")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
 
-tasks.withType<KotlinCompile>() {
+tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "15"
 }
 
---
+++ src/main/kotlin/me/nfekete/adventofcode/y2021/day08/Day08.kt
@@ -0,0 +1,100 @@
+package me.nfekete.adventofcode.y2021.day08
+
+import me.nfekete.adventofcode.y2021.common.classpathFile
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+private enum class Segment {
+    a, b, c, d, e, f, g
+}
+private typealias Digit = Set<Segment>
+
+private fun String.toDigit() = map { Segment.valueOf("$it") }.toSet()
+private data class TestCase(
+    val allDigits: Set<Digit>,
+    val displayedDigigts: List<Digit>
+)
+
+private fun TestCase(line: String) =
+    line.split(" | ")
+        .map { digitSequence ->
+            digitSequence.split(" ").map { it.toDigit() }
+        }.let { (left, right) -> TestCase(left.toSet(), right) }
+
+private fun TestCase.one() = allDigits.single { digit -> digit.size == 2 }
+private fun TestCase.seven() = allDigits.single { digit -> digit.size == 3 }
+private fun TestCase.four() = allDigits.single { digit -> digit.size == 4 }
+private fun TestCase.eight() = allDigits.single { digit -> digit.size == 7 }
+private fun TestCase.part1(): Int {
+    val one = one()
+    val seven = seven()
+    val four = four()
+    val eight = eight()
+    val unambiguousDigits = setOf(one, seven, four, eight)
+    return displayedDigigts.filter { it in unambiguousDigits }.size
+}
+
+private fun TestCase.segmentFrequencies() = allDigits.flatten().groupingBy { it }.eachCount()
+private fun Map<Segment, Int>.whereCountIs(count: Int) = filter { it.value == count }
+
+private fun TestCase.part2(): Int {
+    val one = one()
+    val seven = seven()
+    val four = four()
+    val eight = eight()
+
+    val segmentFrequencies = segmentFrequencies()
+
+    // 9 -> f
+    // 8 -> a, c
+    // 7 -> d, g
+    // 6 -> b
+    // 5 -> e
+
+    val a = (seven - one).single()
+    val b = segmentFrequencies.whereCountIs(6).keys.single()
+    val f = segmentFrequencies.whereCountIs(9).keys.single()
+    val c = (one - f).single()
+    val e = segmentFrequencies.whereCountIs(4).keys.single()
+    val dg = segmentFrequencies.whereCountIs(7).keys
+    val g = (dg - four).single()
+    val d = (dg - g).single()
+
+    val zero = setOf(a, b, c, e, f, g)
+    val two = setOf(a, c, d, e, g)
+    val three = setOf(a, c, d, f, g)
+    val five = setOf(a, b, d, f, g)
+    val six = setOf(a, b, d, e, f, g)
+    val nine = setOf(a, b, c, d, f, g)
+
+
+    val digitMapping = listOf(zero, one, two, three, four, five, six, seven, eight, nine)
+        .withIndex()
+        .associate { (index, value) -> value to index }
+
+    return displayedDigigts
+        .map { digitMapping[it]!! }
+        .fold(0) { acc, digit -> acc * 10 + digit }
+}
+
+
+private fun main() {
+    val input = classpathFile("day08/input.txt")
+        .readLines()
+        .map(::TestCase)
+
+    input.sumOf { it.part1() }.let { println("Part1: $it") }
+    input.sumOf { it.part2() }.let { println("Part2: $it") }
+
+}
+
+internal class Day08KtTest {
+
+    @Test
+    fun testPart2() {
+        val actual = TestCase("acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf")
+            .part2()
+        Assertions.assertEquals(5353, actual)
+    }
+
+}
