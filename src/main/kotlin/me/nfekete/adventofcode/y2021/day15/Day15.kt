e3549461254f6d2a20be7d0c19d332c0a2a55984
Nándor Előd Fekete <nandor.fekete@gmail.com>
2021-12-16 02:20:44 +0100

    Day 15 part 2

--- src/main/kotlin/me/nfekete/adventofcode/y2021/day15/Day15.kt
+++ src/main/kotlin/me/nfekete/adventofcode/y2021/day15/Day15.kt
@@ -22,7 +22,8 @@ private interface Grid {
         val costMap = mutableMapOf(source to 0)
         val prevMap = mutableMapOf<Coord, Coord>()
 
-        val queue = PriorityQueue<Coord>(nullsLast(compareBy { costMap[it] })).apply { add(source) }
+        val queue = PriorityQueue<Coord>(nullsLast(compareBy { costMap[it] }))
+            .apply { add(source) }
         while (queue.isNotEmpty()) {
             val current = queue.remove()
             val costToCurrent = costMap[current]!!
@@ -50,15 +51,32 @@ private class SimpleGrid(val enteringCosts: List<List<Int>>) : Grid {
         get() = enteringCosts.first().indices
     override val yRange: IntRange
         get() = enteringCosts.indices
+
     override fun get(coord: Coord) = enteringCosts[coord.y][coord.x]
+}
+
+private class ComplexGrid(val enteringCosts: List<List<Int>>) : Grid {
+    override val xRange: IntRange
+        get() = 0 until enteringCosts.first().size * 5
+    override val yRange: IntRange
+        get() = 0 until enteringCosts.size * 5
+
+    override fun get(coord: Coord): Int {
+        val divx = coord.x / enteringCosts.first().size
+        val divy = coord.y / enteringCosts.size
+        val modx = coord.x % enteringCosts.first().size
+        val mody = coord.y % enteringCosts.size
+        return (enteringCosts[mody][modx] + divx + divy - 1) % 9 + 1
+    }
 }
 
 private fun main() {
     val input = classpathFile("day15/input.txt")
         .readLines()
         .map { line -> line.map { char -> char.digitToInt() } }
-        .let(::SimpleGrid)
 
-    input.shortestPathTopLeftToBottomRight()
+    SimpleGrid(input).shortestPathTopLeftToBottomRight()
         .let { println("Part1: $it") }
+    ComplexGrid(input).shortestPathTopLeftToBottomRight()
+        .let { println("Part2: $it") }
 }
