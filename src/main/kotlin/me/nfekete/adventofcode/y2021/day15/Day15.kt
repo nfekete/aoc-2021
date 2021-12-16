8a5b41c3dd6fbbb16d8371abcb287db25877eccf
Nándor Előd Fekete <nandor.fekete@gmail.com>
2021-12-16 01:55:24 +0100

    Day 15 part 1 refactor

--- src/main/kotlin/me/nfekete/adventofcode/y2021/day15/Day15.kt
+++ src/main/kotlin/me/nfekete/adventofcode/y2021/day15/Day15.kt
@@ -11,41 +11,54 @@ private fun Coord.getNeighbors() = listOf(
     copy(y = y - 1),
     copy(y = y + 1),
 )
-private typealias Grid = List<List<Int>>
 
-private val Grid.xRange get() = first().indices
-private val Grid.yRange get() = indices
-private fun Grid.isValidCoord(coord: Coord) = coord.x in xRange && coord.y in yRange
-private operator fun Grid.get(coord: Coord) = this[coord.y][coord.x]
+private interface Grid {
+    val xRange: IntRange
+    val yRange: IntRange
+    fun isValidCoord(coord: Coord) = coord.x in xRange && coord.y in yRange
+    operator fun get(coord: Coord): Int
 
-private fun Grid.shortestPath(source: Coord, target: Coord): Int {
-    val costMap = mutableMapOf(source to 0)
-    val prevMap = mutableMapOf<Coord, Coord>()
+    private fun shortestPath(source: Coord, target: Coord): Int {
+        val costMap = mutableMapOf(source to 0)
+        val prevMap = mutableMapOf<Coord, Coord>()
 
-    val queue = PriorityQueue<Coord>(nullsLast(compareBy { costMap[it] })).apply { add(source) }
-    while (queue.isNotEmpty()) {
-        val current = queue.remove()
-        val costToCurrent = costMap[current]!!
-        current.getNeighbors()
-            .filter(::isValidCoord)
-            .forEach { neighbor ->
-                val newCost = costToCurrent + get(neighbor)
-                val oldCost = costMap[neighbor]
-                if (oldCost == null || newCost < oldCost) {
-                    costMap[neighbor] = newCost
-                    prevMap[neighbor] = current
-                    queue.offer(neighbor)
+        val queue = PriorityQueue<Coord>(nullsLast(compareBy { costMap[it] })).apply { add(source) }
+        while (queue.isNotEmpty()) {
+            val current = queue.remove()
+            val costToCurrent = costMap[current]!!
+            current.getNeighbors()
+                .filter(::isValidCoord)
+                .forEach { neighbor ->
+                    val newCost = costToCurrent + get(neighbor)
+                    val oldCost = costMap[neighbor]
+                    if (oldCost == null || newCost < oldCost) {
+                        costMap[neighbor] = newCost
+                        prevMap[neighbor] = current
+                        queue.offer(neighbor)
+                    }
                 }
-            }
+        }
+        return costMap[target]!!
     }
-    return costMap[target]!!
+
+    fun shortestPathTopLeftToBottomRight() =
+        shortestPath(Coord(xRange.first, yRange.first), Coord(xRange.last, yRange.last))
+}
+
+private class SimpleGrid(val enteringCosts: List<List<Int>>) : Grid {
+    override val xRange: IntRange
+        get() = enteringCosts.first().indices
+    override val yRange: IntRange
+        get() = enteringCosts.indices
+    override fun get(coord: Coord) = enteringCosts[coord.y][coord.x]
 }
 
 private fun main() {
     val input = classpathFile("day15/input.txt")
         .readLines()
         .map { line -> line.map { char -> char.digitToInt() } }
+        .let(::SimpleGrid)
 
-    input.shortestPath(Coord(0, 0), Coord(input.xRange.last, input.yRange.last))
+    input.shortestPathTopLeftToBottomRight()
         .let { println("Part1: $it") }
 }
